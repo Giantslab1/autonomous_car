eeeebd6e08e0e3f20d372efbd33aa908ba3168ee
unknown <thegiantselectronics@gmail.com>
2020-01-14 18:47:37 +0200

    servo motor bluetooth improved

--- servo_blue_ard/servo_blue_ard.ino
+++ servo_blue_ard/servo_blue_ard.ino
@@ -4,11 +4,15 @@
 SoftwareSerial bluetooth(2,3);
 Servo myservo;  
 
-int pos = 0;    
+int pos = 0;
+int rightLed = 11;
+int leftLed = 12;   
 void setup() {
   myservo.attach(10);
   Serial.begin(9600);
   bluetooth.begin(9600);
+  pinMode(rightLed, OUTPUT);
+  pinMode(leftLed, OUTPUT);
 }
 
 void loop() {
@@ -28,6 +32,18 @@ void loop() {
     myservo.write(pos);              
     delay(15);                       
   }
+  }else if(c == 'F'){
+    digitalWrite(rightLed, HIGH);
+    digitalWrite(leftLed, LOW);
+  }else if(c == 'E'){
+    digitalWrite(rightLed, LOW);
+    digitalWrite(leftLed, HIGH);
+  }else if(c == 'D'){
+    digitalWrite(rightLed, LOW);
+    digitalWrite(leftLed, LOW);
+  }else if(c == 'C'){
+    digitalWrite(rightLed, LOW);
+    digitalWrite(leftLed, LOW);
   }
   }
   
